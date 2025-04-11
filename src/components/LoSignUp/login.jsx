c757a2df7ad9f99ef6bde110030e0218d969024d
dev3robin <professionalrobin535@gmail.com>
2025-04-11 21:34:33 +0600

    last update

--- src/components/LoSignUp/login.jsx
+++ src/components/LoSignUp/login.jsx
@@ -5,7 +5,7 @@ const Login= () => {
   
   return (
     <div className="login-Container logsign">
-      <div><img src="./public/logSign-logo/signupImg.png" alt="" /></div>
+      <div><img src="/logSign-logo/signupImg.png" alt="" /></div>
       <div className="loginCard lscard">
         <div className="loginInput inclass">
           <div className="heading"><h1>Log in continue your learning journey</h1></div>
@@ -14,10 +14,10 @@ const Login= () => {
         </div>
         <div className='hr'><hr /><span>Other log in options</span><hr /></div>
         <div className="mediaIcon">
-          <button><img src="./public/logSign-logo/google.png" alt="" height='25px'
+          <button><img src="/logSign-logo/google.png" alt="" height='25px'
           width="25px"/></button>
-          <button><img src="./public/logSign-logo/facebook.png"  height='25px'width="25px"alt="" /></button>
-          <button><img src="./public/logSign-logo/apple-logo.png"height='25px' width="25px" alt="" /></button>
+          <button><img src="/logSign-logo/facebook.png"  height='25px'width="25px"alt="" /></button>
+          <button><img src="/logSign-logo/apple-logo.png"height='25px' width="25px" alt="" /></button>
         </div>
         <div className='switching'><span>Dont't have an account</span><Link to="/signUp"><li>Sign up</li></Link></div>
       </div>
--- src/components/LoSignUp/signUp.jsx
+++ src/components/LoSignUp/signUp.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const SignUp = () => {
   return (
     <div className='signup-container logsign'>
-      <div><img src="./public/logSign-logo/signupImg.png" alt="" /></div>
+      <div><img src="/logSign-logo/signupImg.png" alt="" /></div>
       <div className='signup-card lscard'>
         <div className="signupInput inclass">
           <div className='heading'><h1>Sign up with email</h1></div>
@@ -17,10 +17,10 @@ const SignUp = () => {
         </div>
         <div className='hr'><hr /><span>Other sign up options</span><hr /></div>
         <div className="mediaIcon">
-          <button><img src="./public/logSign-logo/google.png" alt="" height='25px'
+          <button><img src="/logSign-logo/google.png" alt="" height='25px'
           width="25px"/></button>
-          <button><img src="./public/logSign-logo/facebook.png"  height='25px'width="25px"alt="" /></button>
-          <button><img src="./public/logSign-logo/apple-logo.png"height='25px' width="25px" alt="" /></button>
+          <button><img src="/logSign-logo/facebook.png"  height='25px'width="25px"alt="" /></button>
+          <button><img src="/logSign-logo/apple-logo.png"height='25px' width="25px" alt="" /></button>
         </div>
         <footer>
           By signing up, you agree to our <span>Terms of use </span> and <span>Privacy policy</span>
